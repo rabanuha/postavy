bb5e256145740f1acdc76cc809291bd09775f2ad
Алекс <33630021+rabanuha@users.noreply.github.com>
2025-07-06 09:20:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 } else {
   // Геолокация не поддерживается
-  console.log("Geolocation is not supported by this browser.");
+ alert("Geolocation is not supported by this browser.");
 }
 
 function successCallback(position) {
@@ -17,16 +17,16 @@ function successCallback(position) {
 function errorCallback(error) {
   switch (error.code) {
     case error.PERMISSION_DENIED:
-      console.log("User denied the request for Geolocation.");
+      alert("User denied the request for Geolocation.");
       break;
     case error.POSITION_UNAVAILABLE:
-      console.log("Location information is unavailable.");
+      alert("Location information is unavailable.");
       break;
     case error.TIMEOUT:
-      console.log("The request to get user location timed out.");
+      alert("The request to get user location timed out.");
       break;
     case error.UNKNOWN_ERROR:
-      console.log("An unknown error occurred.");
+      alert("An unknown error occurred.");
       break;
   }
 }
