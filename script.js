dbc36174b2c207b252c1023a8949c4c5afd5edef
Алекс <33630021+rabanuha@users.noreply.github.com>
2025-07-06 16:07:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,19 +52,19 @@ function successCallback(position) {
   alert("Latitude: " + latitude + ", Longitude: " + longitude);
 }
 
-// function errorCallback(error) {
-//   switch (error.code) {
-//     case error.PERMISSION_DENIED:
-//       alert("User denied the request for Geolocation.");
-//       break;
-//     case error.POSITION_UNAVAILABLE:
-//       alert("Location information is unavailable.");
-//       break;
-//     case error.TIMEOUT:
-//       alert("The request to get user location timed out.");
-//       break;
-//     case error.UNKNOWN_ERROR:
-//       alert("An unknown error occurred.");
-//       break;
-//   }
-// }
+function errorCallback(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      alert("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred.");
+      break;
+  }
+}
