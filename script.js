9cafd2ecb6c649e5b5e8706802b478f836ab7d0b
Алекс <33630021+rabanuha@users.noreply.github.com>
2025-07-06 16:15:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 const currentTime = new Date();
 const currentHour = currentTime.getHours();
 const currentMinute = currentTime.getMinutes();
-
-const timeArray = [
+const busStop = [
+  {name: 'ПТУ', loc: 55.1178117, timeArrivalBus1: [
   { hour: 7, minute: 3 },
   { hour: 7, minute: 33 },
   { hour: 8, minute: 38 },
@@ -23,21 +22,37 @@ const timeArray = [
   { hour: 17, minute: 53 },
   { hour: 18, minute: 13 },
   { hour: 18, minute: 43 },
+]}
 ];
 
+function findObject(array, key, value) {
+  return array.find(item => item[key] === value);
+}
 
 
+// Получаем данные локации
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 } else {
-  // Геолокация не поддерживается
  alert("Geolocation is not supported by this browser.");
 }
-
 function successCallback(position) {
+
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-  alert("Latitude: " + latitude + ", Longitude: " + longitude);
+  const foundObject = findObject(busStop, 'loc', latitude);
+
+  for (let i = 0 ; i < foundObject.timeArrivalBus1.length; i++) {
+      if ( currentHour == foundObject.timeArrivalBus1.hour && (currentMinute < foundObject.timeArrivalBus1.minute) ) {
+        let minuteForBus = foundObject.timeArrivalBus1[i].minute - currentMinute;
+        alert('Ваша остановка ' + foundObject.name + '\nМаршрут №1 \nДо ближайшего автобуса: ' + minuteForBus + ' минут');
+        break;
+      } else if (currentHour < foundObject.timeArrivalBus1[i].hour) {
+        let minuteForBus = (60 - currentMinute) + foundObject.timeArrivalBus1[i].minute;
+        alert('Ваша остановка ' + foundObject.name + '\nМаршрут №1 \nдо ближайшего автобуса: ' + minuteForBus + ' минут');
+        break;
+      }
+  }  
 }
 
 function errorCallback(error) {
@@ -55,16 +70,4 @@ function errorCallback(error) {
       alert("An unknown error occurred.");
       break;
   }
-}
-
-for (let i = 0 ; i < timeArray.length; i++) {
-    if ( currentHour == timeArray[i].hour && (currentMinute < timeArray[i].minute) ) {
-      let minuteForBus = timeArray[i].minute - currentMinute;
-      alert( 'Маршрут №1 \nдо ближайшего автобуса: ' + minuteForBus + ' минут');
-      break;
-    } else if (currentHour < timeArray[i].hour) {
-      let minuteForBus = (60 - currentMinute) + timeArray[i].minute;
-      alert('Маршрут №1 \nдо ближайшего автобуса: ' + minuteForBus + ' минут');
-      break;
-    }
 }
