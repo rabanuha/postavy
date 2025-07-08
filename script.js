16187e3ba6b4fb5061cd31486013daefc57d79c2
Алекс <33630021+rabanuha@users.noreply.github.com>
2025-07-08 13:19:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,27 @@ const busStop = [
   { hour: 17, minute: 23 },
   { hour: 17, minute: 53 },
   { hour: 18, minute: 13 },
-  { hour: 18, minute: 43 },
+  { hour: 18, minute: 43 }
+]}, {name: 'Льнозавод', latitudeBus: 55.1181, longitudeBus: 26.8240, timeArrivalBus1: [
+  { hour: 7, minute: 3 },
+  { hour: 7, minute: 33 },
+  { hour: 8, minute: 38 },
+  { hour: 8, minute: 58 },
+  { hour: 11, minute: 8 },
+  { hour: 11, minute: 33 },
+  { hour: 11, minute: 58 },
+  { hour: 12, minute: 28 },
+  { hour: 12, minute: 53 },
+  { hour: 13, minute: 48 },
+  { hour: 14, minute: 18 },
+  { hour: 15, minute: 13 },
+  { hour: 15, minute: 43 },
+  { hour: 16, minute: 33 },
+  { hour: 17, minute: 3 },
+  { hour: 17, minute: 23 },
+  { hour: 17, minute: 53 },
+  { hour: 18, minute: 13 },
+  { hour: 18, minute: 43 }
 ]}
 ];
 
@@ -102,4 +122,3 @@ function errorCallback(error) {
       break;
   }
 }
-
